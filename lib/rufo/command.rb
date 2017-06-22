23c8143e9530e7b8c891e9690402c5068fb089ca
Ary Borenszweig <asterite@gmail.com>
2017-06-22 18:28:53 -0300

    Add --check to check for formatting changes

--- lib/rufo/command.rb
+++ lib/rufo/command.rb
@@ -1,25 +1,34 @@
+require "optionparser"
+
 module Rufo::Command
   def self.run
+    want_check = parse_options
+
     if ARGV.empty?
-      format_stdin
+      format_stdin want_check
     else
-      format_args ARGV
+      format_args ARGV, want_check
     end
   rescue Rufo::SyntaxError
     STDERR.puts "Error: the given text is not a valid ruby program (it has syntax errors)"
   end
 
-  def self.format_stdin
+  def self.format_stdin(want_check)
     code   = STDIN.read
-    result = format(code, Dir.current)
-    print result
+    result = format(code, Dir.current, want_check)
+
+    if want_check
+      exit 1 if result != code
+    else
+      print result
+    end
   rescue Rufo::Bug => ex
     STDERR.puts "You've found a bug!"
     STDERR.puts "Please report it to https://github.com/asterite/rufo/issues with code that triggers it"
     STDERR.puts
   end
 
-  def self.format_args(args)
+  def self.format_args(args, want_check)
     files = []
 
     args.each do |arg|
@@ -32,19 +41,31 @@ module Rufo::Command
       end
     end
 
+    changed = false
+
     files.each do |file|
-      format_file file
+      changed ||= format_file file, want_check
     end
+
+    exit 1 if changed
   end
 
-  def self.format_file(filename)
+  def self.format_file(filename, want_check)
     code   = File.read(filename)
     result = format(code, File.dirname(filename))
 
     if code != result
-      File.write(filename, result)
-      puts "Format: #{filename}"
+      if want_check
+        STDERR.puts "Error: formatting #{filename} produced changes"
+      else
+        File.write(filename, result)
+        puts "Format: #{filename}"
+      end
+
+      return true
     end
+
+    false
   rescue Rufo::Bug => ex
     STDERR.puts "You've found a bug!"
     STDERR.puts "It happened while trying to format the file #{filename}"
@@ -81,5 +102,24 @@ module Rufo::Command
     return if parent_dir == dir
 
     find_dot_rufo(parent_dir)
+  end
+
+  def self.parse_options
+    want_check = false
+
+    OptionParser.new do |opts|
+      opts.banner = "Usage: rufo files or dirs [options]"
+
+      opts.on("-c", "--check", "Only check formating changes") do
+        want_check = true
+      end
+
+      opts.on("-h", "--help", "Show this help") do
+        puts opts
+        exit
+      end
+    end.parse!
+
+    want_check
   end
 end
