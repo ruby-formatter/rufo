f581ac024c742c1e7d4f49512806488bf0c2dd51
Max Brosnahan <maximilianbrosnahan@gmail.com>
2017-10-02 23:54:33 +0200

    Change spaces_in_ternary config to :one

--- lib/rufo/formatter/settings.rb
+++ lib/rufo/formatter/settings.rb
@@ -3,7 +3,7 @@ class Rufo::Formatter
     indent_size options.fetch(:indent_size, 2)
     spaces_inside_hash_brace options.fetch(:spaces_inside_hash_brace, :dynamic)
     spaces_inside_array_bracket options.fetch(:spaces_inside_array_bracket, :dynamic)
-    spaces_in_ternary options.fetch(:spaces_in_ternary, :dynamic)
+    spaces_in_ternary options.fetch(:spaces_in_ternary, :one)
     spaces_around_binary options.fetch(:spaces_around_binary, :dynamic)
     spaces_in_inline_expressions options.fetch(:spaces_in_inline_expressions, :dynamic)
     parens_in_def options.fetch(:parens_in_def, :dynamic)
