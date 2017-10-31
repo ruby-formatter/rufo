86273365a04a38463fa651fcf587d48c92e4c57e
mjago <martyn.jago@btinternet.com>
2017-10-31 16:09:41 +0000

    Fix formatting error for issue 49

--- lib/rufo/formatter.rb
+++ lib/rufo/formatter.rb
@@ -2966,8 +2966,11 @@ class Rufo::Formatter
 
         write_indent if found_comment
         if current_token_value.end_with?("\n")
+          write_space
           write current_token_value.rstrip
-          write_line
+          write "\n"
+          write_indent(next_indent)
+          @column = next_indent
         else
           write current_token_value
         end
@@ -3615,7 +3618,7 @@ class Rufo::Formatter
         next if @unmodifiable_string_lines[line]
 
         current_line = lines[line]
-        current_line = current_line[diff..-1] if diff >=0
+        current_line = current_line[diff..-1] if diff >= 0
 
         # It can happen that this line didn't need an indent because
         # it simply had a newline
