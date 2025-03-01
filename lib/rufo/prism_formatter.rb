fc62638bcf22251efdb589812e4cf87bfbe07f59
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2025-03-01 13:53:52 +0900

    traverse ast output by prism

--- lib/rufo/prism_formatter.rb
+++ lib/rufo/prism_formatter.rb
@@ -23,14 +23,102 @@ class Rufo::PrismFormatter
   end
 
   def format
-    @output = @code.dup
+    visitor = FormatVisitor.new(@code)
+    @parse_result.value.accept(visitor)
+    @output = visitor.output
 
     @output.chomp! if @output.end_with?("\n\n")
     @output.lstrip!
-    @output = "\n" if @output.empty?
+    @output << "\n" unless @output.end_with?("\n")
   end
 
   def result
     @output
+  end
+
+  class FormatVisitor < Prism::Visitor
+    attr_reader :output
+
+    def initialize(code)
+      super()
+      @code = code
+      @output = +""
+    end
+
+    def visit_nil_node(_node)
+      write("nil")
+    end
+
+    def visit_true_node(_node)
+      write("true")
+    end
+
+    def visit_false_node(_node)
+      write("false")
+    end
+
+    def visit_integer_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_float_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_rational_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_imaginary_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_symbol_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_interpolated_symbol_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_string_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_class_variable_read_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_global_variable_read_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_numbered_reference_read_node(node)
+      write_code_at(node.location)
+    end
+
+    def visit_local_variable_write_node(node)
+      write(node.name.to_s)
+      write(" = ")
+      node.value.accept(self)
+    end
+
+    def visit_hash_node(node)
+      write_code_at(node.location)
+    end
+
+    private
+
+    def write(value)
+      @output << value
+    end
+
+    def write_code_at(location)
+      write(code_at(location))
+    end
+
+    def code_at(location)
+      @code[location.start_offset...location.end_offset]
+    end
   end
 end
